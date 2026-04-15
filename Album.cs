4c15fd4fd9eeb5b4f9578234cfb8a93c8bf31665
Kelvin Moraes Souza <83683962+thecoaty@users.noreply.github.com>
2026-04-14 22:24:50 -0300

    Add files via upload

---
+++ Album.cs
@@ -0,0 +1,22 @@
+﻿class Album
+{
+    private List<Musica> musicas = new List<Musica>();
+    public string Nome { get; set; }
+
+    public int DuracaoTotal => musicas.Sum(m => m.Duracao);
+
+    public void AdicionarMusica(Musica musica)
+    {
+        musicas.Add(musica);
+    }
+
+    public void ExibirAlbum()
+    {
+        Console.WriteLine($"Album: {Nome}");
+        foreach (var musica in musicas)
+        {
+            Console.WriteLine(musica.Nome);
+        }
+        Console.WriteLine($"A duração total do album é {DuracaoTotal}");
+    }
+}
