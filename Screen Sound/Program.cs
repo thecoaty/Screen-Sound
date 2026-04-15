3b9c29e7847a066cdc7a0c740041b09fae8d69c9
Coaty <Myheartofglass@hotmail.com>
2026-04-14 21:28:21 -0300

    add albuns

--- Screen Sound/Program.cs
+++ Screen Sound/Program.cs
@@ -1,14 +1,16 @@
-﻿Musica musica1 = new Musica();
-musica1.Nome = "Trenchcoat";
-musica1.Artista = "Ghostemane";
-musica1.Duracao = 314;
-musica1.Disponivel = true;
+﻿Album LilPeepPartOne = new Album();
 
-Musica musica2 = new Musica();
-musica2.Nome = "Gym class";
-musica2.Artista = "Lil Peep";
-musica2.Duracao = 322;
-musica2.Disponivel = true;
+LilPeepPartOne.Nome = "Part One";
 
-musica1.ExibirFichaTecnica();
-musica2.ExibirFichaTecnica();
+Musica musicaPeep1 = new Musica();
+musicaPeep1.Nome = "praying to the sky";
+musicaPeep1.Duracao = 239;
+
+Musica musicaPeep2 = new Musica();
+musicaPeep2.Nome = "the way i see things";
+musicaPeep2.Duracao = 133;
+
+LilPeepPartOne.AdicionarMusica(musicaPeep1);
+LilPeepPartOne.AdicionarMusica(musicaPeep2);
+
+LilPeepPartOne.ExibirAlbum();
